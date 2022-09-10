11c7d70af7ad54f978d44e1ddf4180413a7be9d9
Saul Henriquez <saul_henriquez@hotmail.com>
2022-09-10 16:37:27 -0600

    Remove not implemented methods

--- homefeeder-sdk/src/main/java/ua/itaysonlab/hfsdk/FeedItem.kt
+++ homefeeder-sdk/src/main/java/ua/itaysonlab/hfsdk/FeedItem.kt
@@ -12,12 +12,4 @@ data class FeedItem(
     val type: FeedItemType,
     val content: BaseContent,
     val time: Long
-): Parcelable {
-    override fun describeContents(): Int {
-        TODO("Not yet implemented")
-    }
-
-    override fun writeToParcel(p0: Parcel?, p1: Int) {
-        TODO("Not yet implemented")
-    }
-}
+): Parcelable
