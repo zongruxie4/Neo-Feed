717e22f953f510bbc281bc54c58e8b172c836f26
Saul Henriquez <saul_henriquez@hotmail.com>
2022-09-10 17:20:12 -0600

    Add theme colors

---
+++ homefeeder-rss/src/main/java/com/saulhdev/ofrss/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.saulhdev.ofrss.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme
+val LightBackground = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF000000)
+val LightSurface = Color(0xFFE9ECEF)
+val LightOnSurface = Color(0xFF191C1A)
+val LightPrimary = Color(0xFFFF1744)
+val LightOnPrimary = Color(0xFF434343)
+val LightSurfaceVariant = Color(0xFFE9ECEF)
+val LightOnSurfaceVariant = Color(0xFF555555)
+val LightOutline = Color(0xff9d9d9d)
