da00783badab947bf37f9576c8526316692ec2a7
Saul <saul_henriquez@hotmail.com>
2022-09-11 14:27:27 -0600

    implements Parcelable methods
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- homefeeder-sdk/src/main/java/ua/itaysonlab/hfsdk/content/StoryCardContent.kt
+++ homefeeder-sdk/src/main/java/ua/itaysonlab/hfsdk/content/StoryCardContent.kt
@@ -1,17 +1,22 @@
 package ua.itaysonlab.hfsdk.content
 
+import android.os.Parcel
 import kotlinx.android.parcel.Parcelize
 import ua.itaysonlab.hfsdk.FeedCategory
 
-/**
- * [BaseContent] adapted for [ua.itaysonlab.hfsdk.FeedItemType.STORY_CARD]
- */
-
 @Parcelize
-data class StoryCardContent (
+data class StoryCardContent(
     val title: String,
     val text: String,
-    val background_url: String, // network
+    val background_url: String,
     val link: String,
     val source: FeedCategory
-): BaseContent()
+) : BaseContent() {
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+
+    }
+}
