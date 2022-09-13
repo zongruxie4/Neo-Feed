a22b9684c8635d27585dd5ca1ca40ce903c6527d
Saul Henriquez <saul_henriquez@hotmail.com>
2022-09-12 21:42:43 -0600

    Rework NavigationManager
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- app/src/main/java/com/saulhdev/feeder/compose/navigation/NavigationManager.kt
+++ app/src/main/java/com/saulhdev/feeder/compose/navigation/NavigationManager.kt
@@ -18,58 +18,25 @@
 
 package com.saulhdev.feeder.compose.navigation
 
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.unit.LayoutDirection
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import com.google.accompanist.navigation.animation.AnimatedNavHost
-import soup.compose.material.motion.materialSharedAxisX
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.saulhdev.feeder.InfoScreen
+import com.saulhdev.feeder.SettingsScreen
+import com.saulhdev.feeder.SourcesScreen
 
-object Routes {
-    const val PREFS_MAIN = "prefs_main"
-}
-
-val LocalNavController = staticCompositionLocalOf<NavController> {
-    error("CompositionLocal LocalNavController not present")
-}
-
-@OptIn(ExperimentalAnimationApi::class)
 @Composable
-fun DefaultComposeView(navController: NavHostController) {
-    val isRtl = LocalLayoutDirection.current == LayoutDirection.Rtl
-    val motionSpec = materialSharedAxisX()
-    val density = LocalDensity.current
-    CompositionLocalProvider(
-        LocalNavController provides navController
-    ) {
-        AnimatedNavHost(
-            navController = navController,
-            startDestination = "/",
-            enterTransition = { motionSpec.enter.transition(!isRtl, density) },
-            exitTransition = { motionSpec.exit.transition(!isRtl, density) },
-            popEnterTransition = { motionSpec.enter.transition(isRtl, density) },
-            popExitTransition = { motionSpec.exit.transition(isRtl, density) },
-        ) {
-            preferenceGraph(route = "/", { BlankScreen() })
+fun NavigationManager(navController: NavHostController) {
+    NavHost(navController, startDestination = NavigationItem.Settings.route) {
+        composable(NavigationItem.Sources.route) {
+            SourcesScreen()
         }
-    }
-}
-
-@Composable
-fun BlankScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .fillMaxHeight()
-    ) {
+        composable(NavigationItem.Settings.route) {
+            SettingsScreen()
+        }
+        composable(NavigationItem.Info.route) {
+            InfoScreen()
+        }
     }
 }
