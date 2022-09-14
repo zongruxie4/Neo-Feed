897426c70101dc5cd33daefca69132da0515784a
Saul <saul_henriquez@hotmail.com>
2022-09-13 21:23:17 -0600

    Add PreferenceBuilder
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

---
+++ app/src/main/java/com/saulhdev/feeder/compose/components/PreferenceBuilder.kt
@@ -0,0 +1,31 @@
+/*
+ * This file is part of Neo Feeder
+ * Copyright (c) 2022   Saul Henriquez <henriquez.saul@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saulhdev.feeder.compose.components
+
+import androidx.compose.runtime.Composable
+import com.saulhdev.feeder.preference.FeedPreferences
+
+val PreferenceBuilder = @Composable { pref: Any, index: Int, size: Int ->
+    when (pref) {
+        is FeedPreferences.StringSetPref ->
+            StringSetPreference(pref = pref, index = index, groupSize = size)
+        is FeedPreferences.StringPref ->
+            ActionPreference(pref = pref, index = index, groupSize = size)
+    }
+}
