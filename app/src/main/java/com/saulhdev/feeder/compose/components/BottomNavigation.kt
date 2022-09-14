100f3577c6f3fbdc2162491ce956aa5f6305f078
Saul <saul_henriquez@hotmail.com>
2022-09-13 21:16:19 -0600

    Add BottomNavigation
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

---
+++ app/src/main/java/com/saulhdev/feeder/compose/components/BottomNavigation.kt
@@ -0,0 +1,84 @@
+/*
+ * This file is part of Omega Feeder
+ * Copyright (c) 2022   Saul Henriquez <henriquez.saul@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.saulhdev.feeder.compose.components
+
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.saulhdev.feeder.compose.anim.AnimatedIcon
+import com.saulhdev.feeder.compose.navigation.NavigationItem
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        NavigationItem.Sources,
+        NavigationItem.Settings,
+        NavigationItem.Info
+    )
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colorScheme.background,
+        contentColor = Color.White
+    ) {
+        var selectedIndex by remember { mutableStateOf(0) }
+
+        items.forEachIndexed { index, item ->
+            BottomNavigationItem(
+                icon = {
+                    AnimatedIcon(
+                        navItem = item,
+                        scale = if (selectedIndex == index) 1.3f else 1.0f,
+                        color = if (selectedIndex == index) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.outline,
+                        iconSize = 30.dp
+                    ) {
+                        navController.navigate(item.route) {
+                            navController.graph.startDestinationRoute?.let { route ->
+                                popUpTo(route) {
+                                    saveState = true
+                                }
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                        selectedIndex = index
+                    }
+                },
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = false,
+                onClick = {
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                    selectedIndex = index
+                }
+            )
+        }
+    }
+}
