2f225d65fb1a3cac4879a46fa1130b226616073d
Saul <saul_henriquez@hotmail.com>
2022-09-13 21:23:04 -0600

    Add BasePreference
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

---
+++ app/src/main/java/com/saulhdev/feeder/compose/components/BasePreference.kt
@@ -0,0 +1,112 @@
+/*
+ * This file is part of Neo Launcher
+ * Copyright (c) 2022   Neo Launcher Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.saulhdev.feeder.compose.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.saulhdev.feeder.utils.addIf
+
+@Composable
+fun BasePreference(
+    modifier: Modifier = Modifier,
+    @StringRes titleId: Int,
+    @StringRes summaryId: Int = -1,
+    summary: String? = null,
+    isEnabled: Boolean = true,
+    index: Int = 1,
+    groupSize: Int = 1,
+    startWidget: (@Composable () -> Unit)? = null,
+    endWidget: (@Composable () -> Unit)? = null,
+    bottomWidget: (@Composable () -> Unit)? = null,
+    onClick: (() -> Unit)? = null,
+) {
+    val base = index.toFloat() / groupSize
+    val rank = (index + 1f) / groupSize
+
+    Column(
+        modifier = Modifier
+            .clip(
+                RoundedCornerShape(
+                    topStart = if (base == 0f) 16.dp else 6.dp,
+                    topEnd = if (base == 0f) 16.dp else 6.dp,
+                    bottomStart = if (rank == 1f) 16.dp else 6.dp,
+                    bottomEnd = if (rank == 1f) 16.dp else 6.dp
+                )
+            )
+            .background(MaterialTheme.colorScheme.surfaceColorAtElevation((rank * 24).dp))
+            .heightIn(min = 64.dp)
+            .addIf(onClick != null) {
+                clickable(enabled = isEnabled, onClick = onClick!!)
+            }, verticalArrangement = Arrangement.Center
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 8.dp, vertical = 12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            startWidget?.let {
+                startWidget()
+                Spacer(modifier = Modifier.requiredWidth(8.dp))
+            }
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .addIf(!isEnabled) {
+                        alpha(0.3f)
+                    }
+            ) {
+                Text(
+                    text = stringResource(id = titleId),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontSize = 16.sp
+                )
+                if (summaryId != -1 || summary != null) {
+                    Text(
+                        text = summary ?: stringResource(id = summaryId),
+                        color = MaterialTheme.colorScheme.onSurfaceVariant,
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                }
+                bottomWidget?.let {
+                    Spacer(modifier = Modifier.requiredWidth(8.dp))
+                    bottomWidget()
+                }
+            }
+            endWidget?.let {
+                Spacer(modifier = Modifier.requiredWidth(8.dp))
+                endWidget()
+            }
+        }
+    }
+}
