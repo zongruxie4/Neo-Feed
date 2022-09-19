790a887ba8cb8cb5b859797d993a86dd75c81040
Saul <saul_henriquez@hotmail.com>
2022-09-18 19:45:13 -0600

    Remove unused methods
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- app/src/main/java/ua/itaysonlab/homefeeder/HFApplication.kt
+++ app/src/main/java/ua/itaysonlab/homefeeder/HFApplication.kt
@@ -15,56 +15,13 @@ import ua.itaysonlab.homefeeder.utils.OverlayBridge
 class HFApplication : HFPluginApplication() {
     override fun onCreate() {
         super.onCreate()
-        Logger.log(
-            "Application",
-            "Starting HomeFeeder ${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})..."
-        )
         instance = this
         PluginFetcher.init(instance)
     }
 
     companion object {
-        const val ACTION_MANAGE_LISTENERS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"
 
         lateinit var instance: HFApplication
         val bridge = OverlayBridge()
-
-        fun getAppNameByPkg(pkg: String): CharSequence {
-            val ai = try {
-                instance.packageManager.getApplicationInfo(pkg, 0)
-            } catch (e: PackageManager.NameNotFoundException) {
-                null
-            }
-            return if (ai != null) instance.packageManager.getApplicationLabel(ai) else "Unknown"
-        }
-
-        fun getSmallIcon(notification: Notification, pkg: String): Drawable? {
-            return try {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    notification.smallIcon.loadDrawable(instance)
-                } else {
-                    instance.packageManager.getResourcesForApplication(pkg)
-                        .getDrawable(notification.icon, null)
-                }
-            } catch (e: Exception) {
-                instance.packageManager.getApplicationIcon(pkg)
-            }
-        }
-
-        fun getSmallIcon(pkg: String): Drawable {
-            return instance.packageManager.getApplicationIcon(pkg)
-        }
-
-        fun getLargeIcon(notification: Notification): BitmapDrawable? {
-            return try {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    notification.getLargeIcon().loadDrawable(instance) as? BitmapDrawable
-                } else {
-                    notification.largeIcon.toDrawable(instance.resources)
-                }
-            } catch (e: Exception) {
-                null
-            }
-        }
     }
 }
