84626fc1ffc4c54abe27fadc69be3f4babe4a997
Saul Henriquez <saul_henriquez@hotmail.com>
2022-09-20 20:48:15 -0600

    Optimize imports
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- app/src/main/java/ua/itaysonlab/homefeeder/HFApplication.kt
+++ app/src/main/java/ua/itaysonlab/homefeeder/HFApplication.kt
@@ -1,15 +1,7 @@
 package ua.itaysonlab.homefeeder
 
-import android.app.Notification
-import android.content.pm.PackageManager
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import android.os.Build
-import androidx.core.graphics.drawable.toDrawable
-import com.saulhdev.feeder.BuildConfig
 import ua.itaysonlab.hfsdk.HFPluginApplication
 import ua.itaysonlab.homefeeder.pluginsystem.PluginFetcher
-import ua.itaysonlab.homefeeder.utils.Logger
 import ua.itaysonlab.homefeeder.utils.OverlayBridge
 
 class HFApplication : HFPluginApplication() {
