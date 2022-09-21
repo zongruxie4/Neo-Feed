1bd1891f52bd5b026e49de93748a7bf37a863315
Saul <saul_henriquez@hotmail.com>
2022-09-21 16:41:03 -0600

    Separate TopAppBar to as Compose component
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- app/src/main/java/com/saulhdev/feeder/MainActivity.kt
+++ app/src/main/java/com/saulhdev/feeder/MainActivity.kt
@@ -27,16 +27,34 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Scaffold
-import androidx.compose.material.TopAppBar
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -57,7 +75,13 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.compose.rememberNavController
 import com.prof.rssparser.Channel
 import com.prof.rssparser.Parser
-import com.saulhdev.feeder.compose.components.*
+import com.saulhdev.feeder.compose.components.BottomNavigationBar
+import com.saulhdev.feeder.compose.components.DialogNegativeButton
+import com.saulhdev.feeder.compose.components.DialogPositiveButton
+import com.saulhdev.feeder.compose.components.FeedItem
+import com.saulhdev.feeder.compose.components.PreferenceGroup
+import com.saulhdev.feeder.compose.components.StringSelectionPrefDialogUI
+import com.saulhdev.feeder.compose.components.TopBar
 import com.saulhdev.feeder.compose.navigation.NavigationManager
 import com.saulhdev.feeder.models.SavedFeedModel
 import com.saulhdev.feeder.preference.FeedPreferences
@@ -125,19 +149,6 @@ fun MainScreen() {
             }
         },
         backgroundColor = MaterialTheme.colorScheme.background
-    )
-}
-
-@Composable
-fun TopBar(pageTitle: MutableState<String>) {
-    TopAppBar(
-        title = {
-            Text(
-                text = pageTitle.value,
-                fontSize = 18.sp,
-            )
-        },
-        backgroundColor = MaterialTheme.colorScheme.background,
     )
 }
 
