740294be88b828be105b32969e2f1b8674948d93
Saul <saul_henriquez@hotmail.com>
2022-10-01 21:14:56 -0600

    Init AndroidThreeTen
    Signed-off-by: Saul <saul_henriquez@hotmail.com>

--- app/src/main/java/ua/itaysonlab/homefeeder/HFApplication.kt
+++ app/src/main/java/ua/itaysonlab/homefeeder/HFApplication.kt
@@ -1,5 +1,6 @@
 package ua.itaysonlab.homefeeder
 
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.saulhdev.feeder.utils.OverlayBridge
 import ua.itaysonlab.hfsdk.HFPluginApplication
 import ua.itaysonlab.homefeeder.pluginsystem.PluginFetcher
@@ -7,6 +8,7 @@ import ua.itaysonlab.homefeeder.pluginsystem.PluginFetcher
 class HFApplication : HFPluginApplication() {
     override fun onCreate() {
         super.onCreate()
+        AndroidThreeTen.init(this)
         instance = this
         PluginFetcher.init(instance)
     }
