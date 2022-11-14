b7578eeb3210857ae9a7acbada3ee8b065dafa4b
Saul Henriquez <saul_henriquez@hotmail.com>
2022-11-13 18:00:20 -0600

    Add youtube icon
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- homefeeder-sdk/src/main/java/ua/itaysonlab/hfsdk/FeedItem.kt
+++ homefeeder-sdk/src/main/java/ua/itaysonlab/hfsdk/FeedItem.kt
@@ -6,6 +6,7 @@ import ua.itaysonlab.hfsdk.content.BaseContent
 
 @Parcelize
 data class FeedItem(
+    val id: Long,
     val title: String,
     val type: FeedItemType,
     val content: BaseContent,
