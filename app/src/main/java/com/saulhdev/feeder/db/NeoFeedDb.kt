0287ba48ef86ec3be13acc82bf5b11cfa0dad01b
Saul Henriquez <saul_henriquez@hotmail.com>
2023-01-28 08:43:30 -0600

    Add IsEnabledColumn
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- app/src/main/java/com/saulhdev/feeder/db/NeoFeedDb.kt
+++ app/src/main/java/com/saulhdev/feeder/db/NeoFeedDb.kt
@@ -33,7 +33,7 @@ const val ID_UNSET: Long = 0
         Feed::class,
         FeedArticle::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(Converters::class)
@@ -59,7 +59,7 @@ abstract class NeoFeedDb : RoomDatabase() {
     }
 }
 
-val allMigrations = arrayOf(MIGRATION_1_2)
+val allMigrations = arrayOf(MIGRATION_1_2, MIGRATION_2_3)
 
 @Suppress("ClassName")
 object MIGRATION_1_2 : Migration(1, 2) {
@@ -67,6 +67,17 @@ object MIGRATION_1_2 : Migration(1, 2) {
         database.execSQL(
             """
             ALTER TABLE Feeds ADD COLUMN fullTextByDefault INTEGER NOT NULL DEFAULT 0
+            """.trimIndent()
+        )
+    }
+}
+
+@Suppress("ClassName")
+object MIGRATION_2_3 : Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            """
+            ALTER TABLE Feeds ADD COLUMN isEnabled INTEGER NOT NULL DEFAULT 1
             """.trimIndent()
         )
     }
