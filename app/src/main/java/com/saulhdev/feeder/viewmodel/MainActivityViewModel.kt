fa097ccdd109ff4aee5d8e7ac798f501e1511148
Saul Henriquez <saul_henriquez@hotmail.com>
2023-07-09 11:33:32 -0600

    Remove unused MainActivityViewModel
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- app/src/main/java/com/saulhdev/feeder/viewmodel/MainActivityViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.saulhdev.feeder.viewmodel
-
-import com.saulhdev.feeder.models.Repository
-import org.kodein.di.DI
-import org.kodein.di.instance
-
-class MainActivityViewModel(di: DI) : DIAwareViewModel(di) {
-    private val repository: Repository by instance()
-}
