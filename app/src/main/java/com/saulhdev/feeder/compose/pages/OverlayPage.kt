065afc785b987bf8056850012ff12ff6ad767330
Saul Henriquez <saul_henriquez@hotmail.com>
2023-09-05 21:05:04 -0600

    show pull request indicator at center top
    Signed-off-by: Saul Henriquez <saul_henriquez@hotmail.com>

--- app/src/main/java/com/saulhdev/feeder/compose/pages/OverlayPage.kt
+++ app/src/main/java/com/saulhdev/feeder/compose/pages/OverlayPage.kt
@@ -26,6 +26,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -85,7 +86,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
 import org.kodein.di.compose.LocalDI
-import java.time.LocalDateTime
+import org.threeten.bp.LocalDateTime
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -308,12 +309,14 @@ fun OverlayPage() {
             if (isRefreshing) {
                 item {
                     Row(
-                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
                     ) {
 
                         PullRefreshIndicator(
                             refreshing = isRefreshing,
-                            state = pullRefreshState
+                            state = pullRefreshState,
+                            contentColor = MaterialTheme.colorScheme.primary
                         )
                     }
                 }
