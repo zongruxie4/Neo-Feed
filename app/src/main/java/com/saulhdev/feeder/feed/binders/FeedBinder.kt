86b52eaec4e4a1b275d5d27ded8e6119b00811ba
machiav3lli <antonios.hazim@tutanota.com>
2023-11-25 03:14:19 +0100

    Remove: Legacy classes/resources

--- app/src/main/java/com/saulhdev/feeder/feed/binders/FeedBinder.kt
+++
@@ -1,9 +0,0 @@
-package com.saulhdev.feeder.feed.binders
-
-import android.util.SparseIntArray
-import android.view.View
-import com.saulhdev.feeder.sdk.FeedItem
-
-interface FeedBinder {
-    fun bind(theme: SparseIntArray?, item: FeedItem, view: View)
-}
