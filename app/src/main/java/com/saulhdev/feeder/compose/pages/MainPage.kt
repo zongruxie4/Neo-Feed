484027540d0825569f80f74e8dcb3650774db61c
machiav3lli <antonios.hazim@tutanota.com>
2025-04-18 16:31:05 +0200

    Update: Make main screen wide screen friendly and adaptive

--- app/src/main/java/com/saulhdev/feeder/compose/pages/MainPage.kt
+++ app/src/main/java/com/saulhdev/feeder/compose/pages/MainPage.kt
@@ -1,34 +1,41 @@
 package com.saulhdev.feeder.compose.pages
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import com.saulhdev.feeder.compose.components.PagerNavBar
 import com.saulhdev.feeder.compose.components.SlidePager
 import com.saulhdev.feeder.compose.navigation.NavItem
+import com.saulhdev.feeder.compose.navigation.NeoNavigationSuiteScaffold
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainPage(pageIndex: Int = 0) {
+    val scope = rememberCoroutineScope()
     val pages = persistentListOf(
         NavItem.Overlay,
         NavItem.Settings,
         NavItem.Sources,
     )
     val pagerState = rememberPagerState(initialPage = pageIndex, pageCount = { pages.size })
+    val currentPageIndex = remember { derivedStateOf { pagerState.currentPage } }
 
-    Scaffold(
-        containerColor = MaterialTheme.colorScheme.background,
-        contentColor = MaterialTheme.colorScheme.onBackground,
-        bottomBar = { PagerNavBar(pageItems = pages, pagerState = pagerState) },
-    ) { paddingValues ->
+    NeoNavigationSuiteScaffold(
+        pages = pages,
+        currentState = currentPageIndex,
+        onItemClick = { index ->
+            scope.launch {
+                pagerState.animateScrollToPage(index)
+            }
+        }
+    ) {
         SlidePager(
             modifier = Modifier
-                .padding(bottom = paddingValues.calculateBottomPadding())
+                //.padding(paddingValues)
                 .fillMaxSize(),
             pagerState = pagerState,
             pageItems = pages,
