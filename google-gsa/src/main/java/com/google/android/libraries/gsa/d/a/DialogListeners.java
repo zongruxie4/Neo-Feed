d44b38e06cb2176e587b97f48ab27fe227f56678
saulhdev <saul_henriquez@hotmail.com>
2025-06-29 12:30:25 -0600

    Remove DialogListeners.java

--- google-gsa/src/main/java/com/google/android/libraries/gsa/d/a/DialogListeners.java
+++
@@ -1,7 +0,0 @@
-package com.google.android.libraries.gsa.d.a;
-
-import android.content.DialogInterface.OnDismissListener;
-import android.content.DialogInterface.OnShowListener;
-
-interface DialogListeners extends OnDismissListener, OnShowListener {
-}
