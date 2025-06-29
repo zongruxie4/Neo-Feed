d988a4ff9285615f8cbe2efb8a2e0ee74489167d
saulhdev <saul_henriquez@hotmail.com>
2025-06-29 12:06:55 -0600

    Convert DialogListener from java to kotlin

---
+++ google-gsa/src/main/java/com/google/android/libraries/gsa/d/a/DialogListeners.kt
@@ -0,0 +1,5 @@
+package com.google.android.libraries.gsa.d.a
+
+import android.content.DialogInterface
+
+interface DialogListeners : DialogInterface.OnDismissListener, DialogInterface.OnShowListener
