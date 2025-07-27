854e1bab497d3b64eeaf6e227c83fccb59329fa5
saulhdev <saul_henriquez@hotmail.com>
2025-07-26 21:05:53 -0600

    Move theme to a new package

--- app/src/main/java/com/saulhdev/feeder/ui/theme/Typography.kt
+++ app/src/main/java/com/saulhdev/feeder/ui/theme/Typography.kt
@@ -1,6 +1,7 @@
-package com.saulhdev.feeder.ui.compose.theme
+package com.saulhdev.feeder.ui.theme
 
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
@@ -10,7 +11,18 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.sp
 import com.saulhdev.feeder.R
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
 
 @Composable
 fun LinkTextStyle(): TextStyle =
