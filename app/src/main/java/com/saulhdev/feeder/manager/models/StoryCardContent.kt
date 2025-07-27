e5be5ecde220d504b1a2f50a0a0c149331e897ff
saulhdev <saul_henriquez@hotmail.com>
2025-07-27 11:27:12 -0600

    Add tags to model to allow tag filter

--- app/src/main/java/com/saulhdev/feeder/manager/models/StoryCardContent.kt
+++ app/src/main/java/com/saulhdev/feeder/manager/models/StoryCardContent.kt
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Neo Feed
+ * Copyright (c) 2025   Neo Feed Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.saulhdev.feeder.manager.models
 
 import android.os.Parcelable
@@ -8,6 +26,7 @@ import kotlinx.parcelize.Parcelize
 data class StoryCardContent(
     val title: String,
     val text: String,
+    val tag: String = "",
     val background_url: String,
     val link: String,
     val source: FeedCategory
