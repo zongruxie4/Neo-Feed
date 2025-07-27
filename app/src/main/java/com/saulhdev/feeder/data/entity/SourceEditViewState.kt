6fe500faed382130db741eea9e96a2638f60e4c2
saulhdev <saul_henriquez@hotmail.com>
2025-07-27 11:44:50 -0600

    Rename EditFeedViewState to SourceEditViewState

---
+++ app/src/main/java/com/saulhdev/feeder/data/entity/SourceEditViewState.kt
@@ -0,0 +1,31 @@
+/*
+ * This file is part of Neo Feed
+ * Copyright (c) 2025   Neo Feed Team
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.saulhdev.feeder.data.entity
+
+import androidx.compose.runtime.Immutable
+
+@Immutable
+data class SourceEditViewState(
+    val title: String = "",
+    val url: String = "",
+    val tag: String = "",
+    val fullTextByDefault: Boolean = true,
+    val isEnabled: Boolean = true,
+)
