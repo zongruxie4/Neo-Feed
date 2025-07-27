499eb58fd373db10312a3079b875d7e649d1a897
saulhdev <saul_henriquez@hotmail.com>
2025-07-27 09:33:35 -0600

    Rename EditFeedViewMode to SourceEditViewModel

--- app/src/main/java/com/saulhdev/feeder/viewmodels/SourceEditViewModel.kt
+++ app/src/main/java/com/saulhdev/feeder/viewmodels/SourceEditViewModel.kt
@@ -1,6 +1,6 @@
 /*
  * This file is part of Neo Feed
- * Copyright (c) 2025   Saul Henriquez <henriquez.saul@gmail.com>
+ * Copyright (c) 2025   Neo Feed Team
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
@@ -19,10 +19,10 @@
 package com.saulhdev.feeder.viewmodels
 
 import androidx.lifecycle.viewModelScope
-import com.saulhdev.feeder.data.repository.FeedRepository
 import com.saulhdev.feeder.data.db.models.Feed
-import com.saulhdev.feeder.manager.models.EditFeedViewState
-import com.saulhdev.feeder.utils.extensions.NeoViewModel
+import com.saulhdev.feeder.data.entity.SourceEditViewState
+import com.saulhdev.feeder.data.repository.SourcesRepository
+import com.saulhdev.feeder.extensions.NeoViewModel
 import com.saulhdev.feeder.utils.sloppyLinkToStrictURL
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -34,8 +34,8 @@ import kotlinx.coroutines.flow.update
 import org.koin.java.KoinJavaComponent.inject
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class EditFeedViewModel : NeoViewModel() {
-    private val repository: FeedRepository by inject(FeedRepository::class.java)
+class SourceEditViewModel : NeoViewModel() {
+    private val repository: SourcesRepository by inject(SourcesRepository::class.java)
 
     private val _feedId: MutableStateFlow<Long> = MutableStateFlow(-1L)
 
@@ -51,11 +51,12 @@ class EditFeedViewModel : NeoViewModel() {
         Feed()
     )
 
-    fun updateFeed(state: EditFeedViewState) {
-        repository.updateFeed(
+    fun updateFeed(state: SourceEditViewState) {
+        repository.updateSource(
             feed = feed.value.copy(
                 title = state.title,
                 url = sloppyLinkToStrictURL(state.url),
+                tag = state.tag,
                 fullTextByDefault = state.fullTextByDefault,
                 isEnabled = state.isEnabled,
             ),
@@ -65,16 +66,16 @@ class EditFeedViewModel : NeoViewModel() {
     }
 
     val viewState = feed.map { feed: Feed ->
-        EditFeedViewState(
+        SourceEditViewState(
             title = feed.title,
             url = feed.url.toString(),
+            tag = feed.tag,
             fullTextByDefault = feed.fullTextByDefault,
             isEnabled = feed.isEnabled
         )
     }.stateIn(
         viewModelScope,
         SharingStarted.Lazily,
-        EditFeedViewState()
+        SourceEditViewState()
     )
 }
-
