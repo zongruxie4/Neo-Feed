7f1394f9233d12b1bc329966c9b76479a96684ea
machiav3lli <antonios.hazim@tutanota.com>
2025-11-08 16:02:29 +0100

    Add: Bookmarked articles to SourceListState

--- app/src/main/java/com/saulhdev/feeder/viewmodels/SourceListViewModel.kt
+++ app/src/main/java/com/saulhdev/feeder/viewmodels/SourceListViewModel.kt
@@ -2,6 +2,8 @@ package com.saulhdev.feeder.viewmodels
 
 import androidx.lifecycle.viewModelScope
 import com.saulhdev.feeder.data.db.models.Feed
+import com.saulhdev.feeder.data.db.models.FeedItem
+import com.saulhdev.feeder.data.repository.ArticleRepository
 import com.saulhdev.feeder.data.repository.SourcesRepository
 import com.saulhdev.feeder.utils.extensions.NeoViewModel
 import com.saulhdev.feeder.utils.sloppyLinkToStrictURL
@@ -14,20 +16,23 @@ import kotlinx.coroutines.plus
 
 class SourceListViewModel(
     private val feedsRepo: SourcesRepository,
+    articleRepo: ArticleRepository,
 ) : NeoViewModel() {
     private val ioScope = viewModelScope.plus(Dispatchers.IO)
 
     val state = combine(
         feedsRepo.getAllSourcesFlow(),
         feedsRepo.getEnabledSources(),
-        feedsRepo.getAllTagsFlow()
-    ) { allFeeds, enabledFeeds, allTags ->
+        feedsRepo.getAllTagsFlow(),
+        articleRepo.getBookmarkedFeedItems()
+    ) { allFeeds, enabledFeeds, allTags, bookmarked ->
         SourceListState(
             allSources = allFeeds,
             enabledSources = enabledFeeds,
             tagsSourcesMap = allTags.plus("").associateWith { tag ->
                 allFeeds.filter { it.tag.contains(tag) }
-            }
+            },
+            bookmarked = bookmarked,
         )
     }.stateIn(
         ioScope,
@@ -68,4 +73,5 @@ data class SourceListState(
     val allSources: List<Feed> = emptyList(),
     val enabledSources: List<Feed> = emptyList(),
     val tagsSourcesMap: Map<String, List<Feed>> = emptyMap(),
+    val bookmarked: List<FeedItem> = emptyList(),
 )
