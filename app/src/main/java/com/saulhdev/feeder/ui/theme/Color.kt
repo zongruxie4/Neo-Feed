0e3f8dcae5480a061791752da2121fb74fae8d22
machiav3lli <antonios.hazim@tutanota.com>
2026-09-23 02:46:50 +0200

    Add: Colors & strings for new app theme pref

--- app/src/main/java/com/saulhdev/feeder/ui/theme/Color.kt
+++ app/src/main/java/com/saulhdev/feeder/ui/theme/Color.kt
@@ -1,6 +1,7 @@
 package com.saulhdev.feeder.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import com.saulhdev.feeder.R
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -9,3 +10,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val presetColors = mapOf(
+    0xFF33DD55 to R.string.color_green,
+    0xFF3344AA to R.string.color_blue,
+    0xFF6644FF to R.string.color_purple,
+    0xFF00BBBB to R.string.color_teal,
+    0xFFFF8811 to R.string.color_orange,
+    0xFFFFBBCC to R.string.color_pink,
+    0xFFCC0044 to R.string.color_red,
+)
